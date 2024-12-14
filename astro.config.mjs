7c1529d3c0bcb05d246e09f50208c95dadbc7187
薛宇建 <azoux0311@gmail.com>
2024-12-14 12:21:45 +0800

    Add sitemap integration and update robots.txt handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import rehypePrettyCode from 'rehype-pretty-code';
 import { siteConfig } from './src/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: siteConfig.site,
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   markdown: {
     rehypePlugins: [
       [rehypePrettyCode, {
