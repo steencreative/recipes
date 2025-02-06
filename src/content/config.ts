34e3d11f25d827bf415fc75f3dc8e5d43bf579d6
steencreative <d.steen712@gmail.com>
2025-02-06 16:22:21 -0600

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const blog = defineCollection({
     description: z.string().optional(),
     date: z.date(),
     tags: z.array(z.string()).optional(),
-    draft: z.boolean().optional(),
+//    draft: z.boolean().optional(),
 //    image: z.string().optional(),
   }),
 });
